cd8b379adcba889b39627a4641283fe58b5195e1
Joel Christner <joel.christner@gmail.com>
2025-09-20 23:53:08 -0700

    Capitalization because why not

--- src/OllamaFlow.Core/Constants.cs
+++ src/OllamaFlow.Core/Constants.cs
@@ -128,24 +128,24 @@ namespace OllamaFlow.Core
         #region Headers
 
         /// <summary>
-        /// Forwarded for header, generally x-forwarded-for.
+        /// Forwarded for header, generally X-Forwarded-For.
         /// </summary>
-        public static string ForwardedForHeader = "x-forwarded-for";
+        public static string ForwardedForHeader = "X-Forwarded-For";
 
         /// <summary>
         /// Request ID header.
         /// </summary>
-        public static string RequestIdHeader = "x-ollamaflow-request";
+        public static string RequestIdHeader = "X-OllamaFlow-Request";
 
         /// <summary>
         /// Backend server ID header.
         /// </summary>
-        public static string BackendServerHeader = "x-ollamaflow-backend";
+        public static string BackendServerHeader = "X-OllamaFlow-Backend";
 
         /// <summary>
         /// Sticky server header.
         /// </summary>
-        public static string StickyServerHeader = "x-ollamaflow-sticky";
+        public static string StickyServerHeader = "X-OllamaFlow-Sticky";
 
         #endregion
     }
